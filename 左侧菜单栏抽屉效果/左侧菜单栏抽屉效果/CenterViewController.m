99822123634eb813a2f4e8d60d39a3e6c352a156
andyysea <601503988@qq.com>
2017-01-11 10:50:52 +0800

    添加需要的文件类

---
+++ 左侧菜单栏抽屉效果/左侧菜单栏抽屉效果/CenterViewController.m
@@ -0,0 +1,37 @@
+//
+//  CenterViewController.m
+//  左侧菜单栏抽屉效果
+//
+//  Created by junde on 2017/1/11.
+//  Copyright © 2017年 junde. All rights reserved.
+//
+
+#import "CenterViewController.h"
+
+@interface CenterViewController ()
+
+@end
+
+@implementation CenterViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
