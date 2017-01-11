1c66e815c7c1d0e95a122ec2c3bec4b0fff34250
andyysea <601503988@qq.com>
2017-01-11 10:45:40 +0800

    初始化项目

---
+++ 左侧菜单栏抽屉效果/左侧菜单栏抽屉效果/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  左侧菜单栏抽屉效果
+//
+//  Created by junde on 2017/1/11.
+//  Copyright © 2017年 junde. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
